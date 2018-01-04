a0e788fe9dfb37962c09e808cf9393be9d320a47
John2509 <jmarcelofo@gmail.com>
2018-01-04 17:06:34 -0200

    Conjunto de pontuacoes

---
+++ src/sistema/ConjuntoDePontuacoes.java
@@ -0,0 +1,122 @@
+package sistema;
+
+import java.util.ArrayList;
+
+import excecao.TamanhoInvalidoException;
+import pontuacao.Pontuacao;
+
+public class ConjuntoDePontuacoes {
+
+	private ArrayList<Pontuacao> regrasAtivas;
+	private ArrayList<Pontuacao> regrasNaoAtivas;
+	
+	//Cria um Conjunto de pontucoes com um numero nao nulo de elemntos
+	//Se regras for vazio, lanca uma exception
+	public ConjuntoDePontuacoes(ArrayList<Pontuacao> regras) throws TamanhoInvalidoException {
+		if (regras.isEmpty())
+			throw new TamanhoInvalidoException();
+		regrasAtivas = new ArrayList<>();
+		regrasNaoAtivas = new ArrayList<>();
+		for (Pontuacao regra : regras) {
+			if (regra.getAtivo())
+				regrasAtivas.add(regra);
+			else
+				regrasNaoAtivas.add(regra);
+		}
+	}
+
+	//Regras ativas sao aquelas que o programa permite calcular
+	public int getNumeroDeRegrasAtivas() {
+		return regrasAtivas.size();
+	}
+	
+	//Regras nao ativas nao sao calculadas
+	public int getNumeroDeRegrasNaoAtivas() {
+		return regrasNaoAtivas.size();
+	}
+
+	//Adiciona outra regra a lista adequada
+	public void adicionar(Pontuacao regra) {
+		if(regra.getAtivo())
+			regrasAtivas.add(regra);
+		else
+			regrasNaoAtivas.add(regra);
+	}
+
+	//Cria um clone da lista de regras ativas
+	@SuppressWarnings("unchecked")
+	public ArrayList<Pontuacao> getRegrasAtivas() {
+		return (ArrayList<Pontuacao>) regrasAtivas.clone();
+	}
+
+	//Cria um clone da lista de regras nao ativas
+	@SuppressWarnings("unchecked")
+	public ArrayList<Pontuacao> getRegrasNaoAtivas() {
+		return (ArrayList<Pontuacao>) regrasNaoAtivas.clone();
+	}
+
+	//Muda uma regra que pertenca ao conjunto de ativa para nao ativa e vice versa
+	public boolean mudarClassificacaoDeRegra(Pontuacao regra) {
+		if (regrasAtivas.contains(regra)) {
+			regra.setAtivo(false);
+			regrasNaoAtivas.add(regra);
+			regrasAtivas.remove(regra);
+			return true;
+		} else if (regrasNaoAtivas.contains(regra)) {
+			regra.setAtivo(true);
+			regrasAtivas.add(regra);
+			regrasNaoAtivas.remove(regra);
+			return true;
+		}
+		return false;
+	}
+
+	//Retorna a melhor regra ativa para um conjunto de dados
+	public Pontuacao calcularMelhorRegra(ConjuntoDeDados dados) {
+		int maiorPontuacao = 0;
+		int index = 0;
+		for (Pontuacao regra : regrasAtivas) {
+			if(regra.getPontuacao(dados) > maiorPontuacao) {
+				index = regrasAtivas.indexOf(regra);
+			}
+		}
+		if (regrasAtivas.isEmpty())
+			return null;
+		return regrasAtivas.get(index);
+	}
+
+	//Retorna todas as regras que nao tem pontucao 0 para um conjunto de dados
+	public ArrayList<Pontuacao> encontrarRegrasQueGeramPontos(ConjuntoDeDados dados) {
+		ArrayList<Pontuacao> resposta = new ArrayList<>();
+		for (Pontuacao regra : regrasAtivas) {
+			if (regra.condicaoEValida(dados))
+				resposta.add(regra);
+		}
+		return resposta;
+	}
+
+	//Torna todas as regras no conjunto ativas
+	public int ativarTodasAsRegras() {
+		int res = 0;
+		for (Pontuacao regra : this.getRegrasNaoAtivas()) {
+			regra.setAtivo(true);
+			regrasAtivas.add(regra);
+			regrasNaoAtivas.remove(regra);
+			res++;
+		}
+		return res;
+	}
+
+	//Torna todas as regras no conjunto nao ativas
+	public int desativarTodasAsRegras() {
+		int res = 0;
+		for (Pontuacao regra : this.getRegrasAtivas()) {
+			regra.setAtivo(false);
+			regrasNaoAtivas.add(regra);
+			regrasAtivas.remove(regra);
+			res++;
+		}
+		return res;
+	}
+
+}
---
+++ src/testes/TesteConjuntoDePontuacoes.java
@@ -0,0 +1,315 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import excecao.TamanhoInvalidoException;
+import pontuacao.*;
+import sistema.ConjuntoDeDados;
+import sistema.ConjuntoDePontuacoes;
+
+class TesteConjuntoDePontuacoes {
+
+	@Test
+	void deveriaCriarUmConjuntoComSeisRegrasAtivas() {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		regras.add(new Dois());
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		regras.add(new Seis());
+		
+		ConjuntoDePontuacoes conjunto = null;
+		try {
+			conjunto = new ConjuntoDePontuacoes(regras);
+		} catch (TamanhoInvalidoException e) {
+			fail("Caiu em uma excecao quando nao devia");
+		}
+		
+		assertEquals(6, conjunto.getNumeroDeRegrasAtivas());
+		assertEquals(0, conjunto.getNumeroDeRegrasNaoAtivas());
+	}
+	
+	@Test
+	void deveriaCriarUmConjuntoComQuatroRegrasAtivasEDuasNaoAtivas() {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		Pontuacao dois = new Dois();
+		dois.setAtivo(false);
+		regras.add(dois);
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		Pontuacao cinco = new Cincos();
+		cinco.setAtivo(false);
+		regras.add(cinco);
+		regras.add(new Seis());
+		
+		ConjuntoDePontuacoes conjunto = null;
+		try {
+			conjunto = new ConjuntoDePontuacoes(regras);
+		} catch (TamanhoInvalidoException e) {
+			fail("Caiu em uma excecao quando nao devia");
+		}
+		
+		assertEquals(4, conjunto.getNumeroDeRegrasAtivas());
+		assertEquals(2, conjunto.getNumeroDeRegrasNaoAtivas());
+	}
+	
+	@Test
+	void naoDeveriaCriarUmConjuntoVazio() {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		
+		ConjuntoDePontuacoes conjunto = null;
+		try {
+			conjunto = new ConjuntoDePontuacoes(regras);
+			fail("Nao caiu em uma excecao quando devia");
+		} catch (Exception e) {
+			assertEquals(TamanhoInvalidoException.class, e.getClass());
+		}
+		
+		assertNull(conjunto);
+	}
+	
+	@Test
+	void deveriaAdicionarUmaNovaRegraAtivaAoConjunto() throws TamanhoInvalidoException {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		regras.add(new Dois());
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+		
+		conjunto.adicionar(new Seis());
+		
+		assertEquals(6, conjunto.getNumeroDeRegrasAtivas());
+		assertEquals(0, conjunto.getNumeroDeRegrasNaoAtivas());
+	}
+	
+	@Test
+	void deveriaAdicionarUmaNovaRegraNaoAtivaAoConjunto() throws TamanhoInvalidoException {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		Pontuacao dois = new Dois();
+		dois.setAtivo(false);
+		regras.add(dois);
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+		
+		Pontuacao seis = new Seis();
+		seis.setAtivo(false);
+		conjunto.adicionar(seis);
+		
+		assertEquals(4, conjunto.getNumeroDeRegrasAtivas());
+		assertEquals(2, conjunto.getNumeroDeRegrasNaoAtivas());
+	}
+	
+	@Test
+	void deveriaRetornarUmaListaDasRegrasAtivas() throws TamanhoInvalidoException {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		Pontuacao dois = new Dois();
+		dois.setAtivo(false);
+		regras.add(dois);
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		Pontuacao seis = new Seis();
+		seis.setAtivo(false);
+		regras.add(seis);
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+		
+		ArrayList<Pontuacao> resposta = conjunto.getRegrasAtivas();
+		
+		assertEquals(4, resposta.size());
+		assertEquals(Uns.class, resposta.get(0).getClass());
+		assertEquals(Tres.class, resposta.get(1).getClass());
+		assertEquals(Quatros.class, resposta.get(2).getClass());
+		assertEquals(Cincos.class, resposta.get(3).getClass());
+	}
+
+	@Test
+	void deveriaRetornarUmaListaDasRegrasNaoAtivas() throws TamanhoInvalidoException {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		Pontuacao dois = new Dois();
+		dois.setAtivo(false);
+		regras.add(dois);
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		Pontuacao seis = new Seis();
+		seis.setAtivo(false);
+		regras.add(seis);
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+			
+		ArrayList<Pontuacao> resposta = conjunto.getRegrasNaoAtivas();
+		
+		assertEquals(2, resposta.size());
+		assertEquals(Dois.class, resposta.get(0).getClass());
+		assertEquals(Seis.class, resposta.get(1).getClass());
+	}
+	
+	@Test
+	void deveriaMudarUmaRegraDeAtivaParaNaoAtiva() throws TamanhoInvalidoException {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		Pontuacao dois = new Dois();
+		dois.setAtivo(false);
+		regras.add(dois);
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		Pontuacao seis = new Seis();
+		seis.setAtivo(false);
+		regras.add(seis);
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+		ArrayList<Pontuacao> resposta = conjunto.getRegrasAtivas();
+		
+		boolean resultado = conjunto.mudarClassificacaoDeRegra(resposta.get(0));
+		
+		assertTrue(resultado);
+		assertEquals(3, conjunto.getNumeroDeRegrasAtivas());
+		assertEquals(3, conjunto.getNumeroDeRegrasNaoAtivas());
+	}
+	
+	@Test
+	void deveriaMudarUmaRegraDeNaoAtivaParaAtiva() throws TamanhoInvalidoException {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		Pontuacao dois = new Dois();
+		dois.setAtivo(false);
+		regras.add(dois);
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		Pontuacao seis = new Seis();
+		seis.setAtivo(false);
+		regras.add(seis);
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+		ArrayList<Pontuacao> resposta = conjunto.getRegrasNaoAtivas();
+		
+		boolean resultado = conjunto.mudarClassificacaoDeRegra(resposta.get(0));
+		
+		assertTrue(resultado);
+		assertEquals(5, conjunto.getNumeroDeRegrasAtivas());
+		assertEquals(1, conjunto.getNumeroDeRegrasNaoAtivas());
+	}
+	
+	@Test
+	void deveriaNaoMudarUmaRegraSeElaNaoPertenceALista() throws TamanhoInvalidoException {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		Pontuacao dois = new Dois();
+		dois.setAtivo(false);
+		regras.add(dois);
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		Pontuacao seis = new Seis();
+		seis.setAtivo(false);
+		regras.add(seis);
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+		
+		boolean resultado = conjunto.mudarClassificacaoDeRegra(new Seis());
+		
+		assertFalse(resultado);
+		assertEquals(4, conjunto.getNumeroDeRegrasAtivas());
+		assertEquals(2, conjunto.getNumeroDeRegrasNaoAtivas());
+	}
+	
+	@Test 
+	void deveriaEncontrarARegraQueGeraMaisPontos() throws TamanhoInvalidoException {
+		ConjuntoDeDados MockConjunto = Mockito.mock(ConjuntoDeDados.class);
+		int[] valores = {2,1,6,3,6};
+		Mockito.when(MockConjunto.getValores()).thenReturn(valores);
+		Mockito.when(MockConjunto.getNumeroDeLados()).thenReturn(6);
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		regras.add(new Dois());
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		regras.add(new Seis());
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+		
+		Pontuacao resposta = conjunto.calcularMelhorRegra(MockConjunto);
+		
+		assertEquals(12, resposta.getPontuacao(MockConjunto));
+	}
+	
+	@Test 
+	void deveriaCriarUmaListaComTodasAsRegrasQueGeramPontos() throws TamanhoInvalidoException {
+		ConjuntoDeDados MockConjunto = Mockito.mock(ConjuntoDeDados.class);
+		int[] valores = {2,1,6,3,6};
+		Mockito.when(MockConjunto.getValores()).thenReturn(valores);
+		Mockito.when(MockConjunto.getNumeroDeLados()).thenReturn(6);
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		regras.add(new Dois());
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		regras.add(new Seis());
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+		
+		ArrayList<Pontuacao> resposta = conjunto.encontrarRegrasQueGeramPontos(MockConjunto);
+		
+		assertEquals(4, resposta.size());
+		assertEquals(Uns.class, resposta.get(0).getClass());
+		assertEquals(Dois.class, resposta.get(1).getClass());
+		assertEquals(Tres.class, resposta.get(2).getClass());
+		assertEquals(Seis.class, resposta.get(3).getClass());
+	}
+	
+	@Test
+	void deveriaAtivarTodasAsRegras() throws TamanhoInvalidoException {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		Pontuacao dois = new Dois();
+		dois.setAtivo(false);
+		regras.add(dois);
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		Pontuacao seis = new Seis();
+		seis.setAtivo(false);
+		regras.add(seis);
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+		
+		int resposta = conjunto.ativarTodasAsRegras();
+		
+		assertEquals(2, resposta);
+		assertEquals(6, conjunto.getNumeroDeRegrasAtivas());
+		assertEquals(0, conjunto.getNumeroDeRegrasNaoAtivas());
+	}
+	
+	@Test
+	void deveriaDesativarTodasAsRegras() throws TamanhoInvalidoException {
+		ArrayList<Pontuacao> regras = new ArrayList<>();
+		regras.add(new Uns());
+		Pontuacao dois = new Dois();
+		dois.setAtivo(false);
+		regras.add(dois);
+		regras.add(new Tres());
+		regras.add(new Quatros());
+		regras.add(new Cincos());
+		Pontuacao seis = new Seis();
+		seis.setAtivo(false);
+		regras.add(seis);
+		ConjuntoDePontuacoes conjunto = new ConjuntoDePontuacoes(regras);
+		
+		int resposta = conjunto.desativarTodasAsRegras();
+		
+		assertEquals(4, resposta);
+		assertEquals(0, conjunto.getNumeroDeRegrasAtivas());
+		assertEquals(6, conjunto.getNumeroDeRegrasNaoAtivas());
+	}
+}
